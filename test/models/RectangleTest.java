fba3851b569f5f2cea3ad2cede1797e340107045
Beauti <183709979+beautisui@users.noreply.github.com>
2025-05-19 10:59:25 +0530

    Implements ractangle perimeter

--- test/models/RectangleTest.java
+++ test/models/RectangleTest.java
@@ -9,12 +9,27 @@ class RectangleTest {
     void area() {
         Rectangle rectangle = new Rectangle(2, 3);
         int actual = rectangle.area();
-        assertEquals(6,actual);
+        assertEquals(6, actual);
     }
+
     @Test
     void areaWithZeroWidth() {
         Rectangle rectangle = new Rectangle(2, 0);
         int actual = rectangle.area();
-        assertEquals(0,actual);
+        assertEquals(0, actual);
     }
+
+    @Test
+    void perimeter() {
+        Rectangle rectangle = new Rectangle(1, 2);
+        int actual = rectangle.perimeter();
+        assertEquals(6, actual);
+    }
+    @Test
+    void perimeterWithZeros() {
+        Rectangle rectangle = new Rectangle(1, 0);
+        int actual = rectangle.perimeter();
+        assertEquals(2, actual);
+    }
+
 }
